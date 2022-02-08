1983f73d79cee2af2a613aed6b11fa477e345c9b
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2022-02-08 18:11:17 -0500

    added Game entity

---
+++ BballSim.Models/GameModels/GameCreate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BballSim.Models.GameModels
+{
+    public class GameCreate
+    {
+        
+        [ForeignKey("Team"), Required]
+        public int Team1 { get; set; }
+        [ForeignKey("Team"), Required]
+        public int Team2 { get; set; }
+        public int Team1Score { get; set; }
+        public int Team2Score { get; set; }
+        public DateTime GameDate { get; set; }
+    }
+}

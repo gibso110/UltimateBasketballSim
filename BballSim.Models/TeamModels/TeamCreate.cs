a88d5e76c5c1ada071e180206dd7b86c421e4caf
GavinAnkney <gavinmartinankney@gmail.com>
2022-02-08 09:17:57 -0500

    Added team entity and team models

---
+++ BballSim.Models/TeamModels/TeamCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BballSim.Models.TeamModels
+{
+    public class TeamCreate
+    {
+        [Required]
+        public int PlayerId { get; set; }
+        [Required]
+        public string TeamName { get; set; }
+    }
+}
---
+++ BballSim.Models/TeamModels/TeamDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BballSim.Models.TeamModels
+{
+    public class TeamDetail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PlayerId { get; set; }
+        public decimal WLRecord { get; set; }
+        public int GamesPlayed { get; set; }
+
+    }
+}

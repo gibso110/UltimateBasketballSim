1d50ed9398263a90dfbd970568cde81f78b9705a
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2022-02-12 16:56:48 -0500

    game is playing y'all

--- BballSim.Models/GameModels/GameUpdate.cs
+++ BballSim.Models/GameModels/GameUpdate.cs
@@ -4,11 +4,11 @@ namespace BballSim.Models.GameModels
 {
     public class GameUpdate
     {
-        public int GameId { get; set; }
-        public int Team1Id { get; set; }
-        public int Team2Id { get; set; }
+        //public int GameId { get; set; }
+        //public int Team1Id { get; set; }
+        //public int Team2Id { get; set; }
         public int Team1Score { get; set; }
         public int Team2Score { get; set; }
-        public DateTime GameDate { get; set; }
+        //public DateTime GameDate { get; set; }
     }
 }

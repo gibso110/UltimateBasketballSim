f575183e1b1d85399c088455da778375096a6f81
GavinAnkney <gavinmartinankney@gmail.com>
2022-02-22 18:29:18 -0500

    Added annotation to team models

--- BballSim.Models/TeamModels/TeamListItem.cs
+++ BballSim.Models/TeamModels/TeamListItem.cs
@@ -1,10 +1,14 @@
-﻿namespace BballSim.Models.TeamModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BballSim.Models.TeamModels
 {
     public class TeamListItem
     {
         public int TeamId { get; set; }
         public string TeamName { get; set; }
+        [Display(Name = "Win/Loss Record")]
         public int WLRecord { get; set; }
+        [Display(Name = "Games Played")]
         public int GamesPlayed { get; set; }
         //public List<Player> Players { get; set; }
 
